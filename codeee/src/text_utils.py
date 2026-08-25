51694881cf365eafba94a4f74b19fff28447bc67
Muskan <yadavmuski151@gmail.com>
2026-08-25 13:31:23 +0530

    change

--- codeee/src/text_utils.py
+++ codeee/src/text_utils.py
@@ -8,6 +8,3 @@ def capitalize_words(text: str) -> str:
     if not text:
         return ""
     return text.title()
-
-# def reverse_text(text: str) -> str:
-#     return text  # Bug: omitted [::-1] reversal logic
