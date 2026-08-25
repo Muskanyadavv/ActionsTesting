bee52d192f6719f4023e09c9a7a735eac0172a9d
Muskan <yadavmuski151@gmail.com>
2026-08-25 13:28:30 +0530

    correcion in test_text.py

--- codeee/tests/test_text_utils.py
+++ codeee/tests/test_text_utils.py
@@ -2,8 +2,8 @@ from src.text_utils import reverse_text, capitalize_words
 
 
 def test_reverse_text():
-    assert reverse_text("github") == "github"
-    assert reverse_text("12345") == "12345"
+    assert reverse_text("github") == "buhtig"
+    assert reverse_text("12345") == "54321"
 
 
 def test_capitalize_words():
